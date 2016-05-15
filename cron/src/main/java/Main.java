37dbd837cdad1d6b20347d5f2635b2fcc45c68bc
Maciej Makówka <makowka@student.agh.edu.pl>
2016-05-15 02:58:14 +0200

    Almost already done the v 1.0 basic logger
    Signed-off-by: Maciej Makówka <mack9@op.pl>

--- cron/src/main/java/Main.java
+++ cron/src/main/java/Main.java
@@ -1,28 +1,38 @@
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.GeoApiContext;
-import com.google.maps.model.DistanceMatrix;
-import com.google.maps.model.DistanceMatrixElement;
-import com.google.maps.model.DistanceMatrixRow;
+import com.google.maps.model.*;
+import org.joda.time.ReadableInstant;
+import org.json.JSONArray;
 
 public class Main {
 
     public static void main(String args[]){
-        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCzSkC5CdUMz62J0Cg1n6r6BgXJpbwpQi0");
+
+        GeoApiContext context;
+        ContextLoader contextLoader = new ContextLoader();
+        RoutesLoader routesLoader = new RoutesLoader();
+
         try {
-            String[] destinations = new String[1];
-            String[] origins = new String[1];
-            destinations[0] = "Kremerowska 11, Kraków";
-            origins[0] = "Kawiory 21, Kraków";
+            JSONArray loadedRoutes = routesLoader.loadJSON();
+            context = contextLoader.geoApiContextLoader();
 
-//            DistanceMatrixApiRequest distanceMatrixApiRequest = new DistanceMatrixApiRequest(context);
-            DistanceMatrix result = DistanceMatrixApi.getDistanceMatrix(context, destinations, origins ).await();
+            int loadedRoutesAmount = loadedRoutes.length();
 
-            for (DistanceMatrixRow row: result.rows) {
-                for (DistanceMatrixElement element: row.elements) {
-                    String elementResult = String.format("{Distance = %s, Duration = %s, Fare = %s, Status = %s",
-                            element.distance, element.duration, element.fare, element.status);
-                    System.out.println(elementResult);
-                }
+            for (int i = 0; i < loadedRoutesAmount; i++) {
+                String destinations[] = new String[1];
+                String origins[] = new String[1];
+                String id = loadedRoutes.getJSONObject(i).get("id").toString();
+                destinations[0] = loadedRoutes.getJSONObject(i).get("destination").toString();
+                origins[0] = loadedRoutes.getJSONObject(i).get("origin").toString();
+
+//                DistanceMatrixApiRequest distanceMatrixApiRequest = new DistanceMatrixApiRequest(context);
+                TravelMode travelMode = TravelMode.DRIVING;
+
+                DistanceMatrix distanceMatrix = DistanceMatrixApi.getDistanceMatrix(context, destinations, origins).mode(travelMode).await();
+                DirectionsApiRequest directionsApi = DirectionsApi.getDirections(context, destinations[0], origins[0]).alternatives(true);
+                Route route = new Route(id, distanceMatrix, directionsApi);
             }
         } catch (Exception e) {
             e.printStackTrace();
---
+++ cron/src/main/java/Route.java
@@ -0,0 +1,86 @@
+import ch.qos.logback.classic.Logger;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.model.*;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+/**
+ * Created by Maciej on 14.05.2016.
+ * 23:46
+ * Project: 1.
+ *
+ * TODO: Below could be improved.
+ * Please, note that the version 1.0 of this application supports only point to point routes.
+ * You can only enter route with two points - start and end.
+ *
+ */
+public class Route {
+
+//    private String[] origins;
+//    private String[] destinations;
+
+    private String result;
+
+    private String id;
+    private DistanceMatrix distanceMatrix;
+    private DirectionsApiRequest directionsApi;
+    private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+    public Route(String id, DistanceMatrix distanceMatrix, DirectionsApiRequest directionsApi) throws Exception {
+        this.id = id;
+        this.distanceMatrix = distanceMatrix;
+        this.directionsApi = directionsApi;
+        loadRouteInfo();
+        logger.info(this.toString());
+    }
+
+    private void loadRouteInfo() throws Exception {
+
+        DirectionsResult directionsResult = directionsApi.await();
+        DirectionsRoute[] routes = directionsResult.routes;
+
+
+        for (DirectionsRoute route : routes) {
+            // waypoints = System.out.println(route.summary);
+            // ...
+            //System.out.println(route.legs[0].steps[0].startLocation);
+
+        }
+
+        result = buildResult(distanceMatrix.rows[0].elements[0], routes);
+//        for (DistanceMatrixRow row: distanceMatrix.rows) {
+//            for (DistanceMatrixElement element: row.elements) {
+//                result = buildResult(element, routes);
+//
+//            }
+//        }
+    }
+
+    private String buildResult(DistanceMatrixElement me_element, DirectionsRoute[] routes) {
+        String me_result = String.format("\"me_result\": {\"Distance\": \"%s\", \"Duration\": \"%s\", \"DurationInTraffic\": \"%s\", \"Fare\": \"%s\", \"Status\": \"%s\"}",
+                me_element.distance, me_element.duration, me_element.durationInTraffic, me_element.fare, me_element.status);
+//        String r_result = "";
+//        int route_id = 0;
+//        for (DirectionsRoute route : routes){
+//
+//            String route_result = "[summary=" + route.summary + ", legs="
+//                    + Arrays.toString(route.legs) + ", waypoint_order="
+//                    + Arrays.toString(route.waypointOrder) + ", overview_polyline="
+//                    + route.overviewPolyline + ", bounds=" + route.bounds
+//                    + ", warnings=" + Arrays.toString(route.warnings) + "]";
+//
+//            r_result = r_result.concat("\"id\": \"" + route_result + "\",");
+//            route_id++;
+//        }
+
+//        return me_result.concat(" \"r_result\": {" + r_result + "}");
+        return me_result;
+    }
+
+    @Override
+    public String toString() {
+        return "{\"" + id + "\": \"ID\" ," + result + "},\n";
+    }
+}
