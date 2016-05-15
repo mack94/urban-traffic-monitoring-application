402d5bcde02f44e58681684edb5d9b63a8efcd1d
Maciej Makówka <makowka@student.agh.edu.pl>
2016-05-15 03:23:36 +0200

    Final v 1.0 cron
    Signed-off-by: Maciej Makówka <mack9@op.pl>

--- cron/src/main/java/Main.java
+++ cron/src/main/java/Main.java
@@ -2,40 +2,59 @@ import com.google.maps.DirectionsApi;
 import com.google.maps.DirectionsApiRequest;
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.GeoApiContext;
-import com.google.maps.model.*;
-import org.joda.time.ReadableInstant;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.TravelMode;
+import org.joda.time.Instant;
 import org.json.JSONArray;
+
+import java.util.Random;
 
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws InterruptedException {
+
+        Random random = new Random();
 
         GeoApiContext context;
         ContextLoader contextLoader = new ContextLoader();
         RoutesLoader routesLoader = new RoutesLoader();
 
-        try {
-            JSONArray loadedRoutes = routesLoader.loadJSON();
-            context = contextLoader.geoApiContextLoader();
+        while (true) {
+            long wait = random.nextInt(500000) + 25000;
 
-            int loadedRoutesAmount = loadedRoutes.length();
+            try {
+                JSONArray loadedRoutes = routesLoader.loadJSON();
+                context = contextLoader.geoApiContextLoader();
 
-            for (int i = 0; i < loadedRoutesAmount; i++) {
-                String destinations[] = new String[1];
-                String origins[] = new String[1];
-                String id = loadedRoutes.getJSONObject(i).get("id").toString();
-                destinations[0] = loadedRoutes.getJSONObject(i).get("destination").toString();
-                origins[0] = loadedRoutes.getJSONObject(i).get("origin").toString();
+                int loadedRoutesAmount = loadedRoutes.length();
+
+                for (int i = 0; i < loadedRoutesAmount; i++) {
+                    String destinations[] = new String[1];
+                    String origins[] = new String[1];
+                    String id = loadedRoutes.getJSONObject(i).get("id").toString();
+                    destinations[0] = loadedRoutes.getJSONObject(i).get("destination").toString();
+                    origins[0] = loadedRoutes.getJSONObject(i).get("origin").toString();
 
 //                DistanceMatrixApiRequest distanceMatrixApiRequest = new DistanceMatrixApiRequest(context);
-                TravelMode travelMode = TravelMode.DRIVING;
+                    TravelMode travelMode = TravelMode.DRIVING;
+                    Instant departure = Instant.now();
 
-                DistanceMatrix distanceMatrix = DistanceMatrixApi.getDistanceMatrix(context, destinations, origins).mode(travelMode).await();
-                DirectionsApiRequest directionsApi = DirectionsApi.getDirections(context, destinations[0], origins[0]).alternatives(true);
-                Route route = new Route(id, distanceMatrix, directionsApi);
+                    DistanceMatrix distanceMatrix = DistanceMatrixApi
+                            .getDistanceMatrix(context, destinations, origins)
+                            .mode(travelMode)
+                            .departureTime(departure)
+                            .await();
+                    DirectionsApiRequest directionsApi = DirectionsApi
+                            .getDirections(context, destinations[0], origins[0])
+                            .alternatives(true);
+                    Route route = new Route(id, distanceMatrix, directionsApi);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            Thread.sleep(wait);
         }
+
     }
 }
