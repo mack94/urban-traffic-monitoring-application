1f3932736d896490de0e284da44aefdb5630ef33
drzuby <dr.drzuby@gmail.com>
2016-05-18 11:51:23 +0200

    Timer is no longer used

--- cron/src/main/java/Main.java
+++ cron/src/main/java/Main.java
@@ -7,55 +7,45 @@ import com.google.maps.model.TravelMode;
 import org.joda.time.Instant;
 import org.json.JSONArray;
 
-import java.util.Random;
-
 public class Main {
 
     public static void main(String args[]) throws InterruptedException {
-
-        Random random = new Random();
 
         GeoApiContext context;
         ContextLoader contextLoader = new ContextLoader();
         RoutesLoader routesLoader = new RoutesLoader();
 
-        while (true) {
-            long wait = Timer.getWaitingTime();
+        try {
+            JSONArray loadedRoutes = routesLoader.loadJSON();
+            context = contextLoader.geoApiContextLoader();
 
-            try {
-                JSONArray loadedRoutes = routesLoader.loadJSON();
-                context = contextLoader.geoApiContextLoader();
+            int loadedRoutesAmount = loadedRoutes.length();
 
-                int loadedRoutesAmount = loadedRoutes.length();
+            for (int i = 0; i < loadedRoutesAmount; i++) {
+                String destinations[] = new String[1];
+                String origins[] = new String[1];
+                String id = loadedRoutes.getJSONObject(i).get("id").toString();
+                destinations[0] = loadedRoutes.getJSONObject(i).get("destination").toString();
+                origins[0] = loadedRoutes.getJSONObject(i).get("origin").toString();
 
-                for (int i = 0; i < loadedRoutesAmount; i++) {
-                    String destinations[] = new String[1];
-                    String origins[] = new String[1];
-                    String id = loadedRoutes.getJSONObject(i).get("id").toString();
-                    destinations[0] = loadedRoutes.getJSONObject(i).get("destination").toString();
-                    origins[0] = loadedRoutes.getJSONObject(i).get("origin").toString();
+//              DistanceMatrixApiRequest distanceMatrixApiRequest = new DistanceMatrixApiRequest(context);
+                TravelMode travelMode = TravelMode.DRIVING;
+                Instant departure = Instant.now();
 
-//                DistanceMatrixApiRequest distanceMatrixApiRequest = new DistanceMatrixApiRequest(context);
-                    TravelMode travelMode = TravelMode.DRIVING;
-                    Instant departure = Instant.now();
-
-                    DistanceMatrix distanceMatrix = DistanceMatrixApi
-                            .getDistanceMatrix(context, destinations, origins)
-                            .mode(travelMode)
-                            .language("pl")
-                            .departureTime(departure)
-                            .await();
-                    DirectionsApiRequest directionsApi = DirectionsApi
-                            .getDirections(context, destinations[0], origins[0])
-                            .alternatives(true)
-                            .language("pl");
-                    Route route = new Route(id, distanceMatrix, directionsApi);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+                DistanceMatrix distanceMatrix = DistanceMatrixApi
+                        .getDistanceMatrix(context, destinations, origins)
+                        .mode(travelMode)
+                        .language("pl")
+                        .departureTime(departure)
+                        .await();
+                DirectionsApiRequest directionsApi = DirectionsApi
+                        .getDirections(context, destinations[0], origins[0])
+                        .alternatives(true)
+                        .language("pl");
+                Route route = new Route(id, distanceMatrix, directionsApi);
             }
-
-            Thread.sleep(wait);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
