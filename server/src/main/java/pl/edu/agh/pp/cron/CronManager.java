4cf5bcfe1e3e5f800e48adfb31c760148840fa35
drzuby <dr.drzuby@gmail.com>
2016-09-07 20:19:44 +0200

    Cron and detector apps connected into one (server) with two modules.

--- server/src/main/java/pl/edu/agh/pp/cron/CronManager.java
+++ server/src/main/java/pl/edu/agh/pp/cron/CronManager.java
@@ -19,9 +19,9 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Main {
+public class CronManager {
 
-    public static void main(String args[]) throws InterruptedException {
+    public void doSomething() throws InterruptedException {
 
         GeoApiContext context;
         ContextLoader contextLoader = new ContextLoader();
