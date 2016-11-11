1bca229d1820a0b96b172ad27123da5419e58467
Maciej Makówka <makowka@student.agh.edu.pl>
2016-11-11 12:29:22 +0100

    Server missing Connector class from my previous commit added.

---
+++ server/src/main/java/pl/edu/agh/pp/detector/adapters/Connector.java
@@ -0,0 +1,47 @@
+package pl.edu.agh.pp.detector.adapters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.edu.agh.pp.cron.CronManager;
+
+import java.net.InetAddress;
+
+/**
+ * Created by Maciej on 09.11.2016.
+ * 22:09
+ * Project: server.
+ */
+public class Connector {
+
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(Connector.class);
+    private static ChannelReceiver channelReceiver = new ChannelReceiver();
+    private static ManagementServer managementServer = new ManagementServer();
+    private static Server server;
+
+    public static void connect(String[] args, InetAddress bind_addr, int port, boolean nio) throws InterruptedException {
+        managementServer = new ManagementServer();
+        server = new Server();
+        channelReceiver = new ChannelReceiver();
+
+        try {
+            // TODO: I am not sure if we should start both management and anomalies channel both at the same time.
+            managementServer.start(bind_addr, port - 1, nio);
+            server.start(bind_addr, port, nio);
+            channelReceiver.start(bind_addr, port, nio);
+            logger.info("Server already running.");
+        } catch (Exception e) {
+            logger.error("Connector :: Exception " + e, e);
+        }
+
+        Thread.sleep(10000);
+        if(args.length>1)
+            new CronManager(server).doSomething(args[1]);
+        else
+            new CronManager(server).doSomething("");
+    }
+
+    public static void updateLever(double leverValue) {
+        managementServer.sendLeverInfoMessage(leverValue);
+    }
+
+}
