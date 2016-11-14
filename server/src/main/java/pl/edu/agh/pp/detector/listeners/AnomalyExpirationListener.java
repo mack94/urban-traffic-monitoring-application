d2407f6c9926a1e201c05b62bc7449c7ca8bf5eb
drzuby <dr.drzuby@gmail.com>
2016-11-14 21:27:08 +0100

    Notifying about anomaly expiration for 1 hour.

--- server/src/main/java/pl/edu/agh/pp/detector/listeners/AnomalyExpirationListener.java
+++ server/src/main/java/pl/edu/agh/pp/detector/listeners/AnomalyExpirationListener.java
@@ -1,5 +1,9 @@
 package pl.edu.agh.pp.detector.listeners;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.joda.time.DateTime;
 import org.joda.time.Seconds;
 import pl.edu.agh.pp.detector.adapters.Server;
@@ -7,32 +11,37 @@ import pl.edu.agh.pp.detector.operations.AnomalyOperationProtos;
 import pl.edu.agh.pp.settings.Options;
 import pl.edu.agh.pp.settings.exceptions.IllegalPreferenceObjectExpected;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
-public class AnomalyExpirationListener extends Thread {
+public class AnomalyExpirationListener extends Thread
+{
     private ConcurrentHashMap<Integer, Long> anomalyID;
     private ConcurrentHashMap<Integer, DateTime> anomalyTime;
     private Set<Long> expiredAnomalies;
     private Server server;
 
-    public AnomalyExpirationListener(ConcurrentHashMap<Integer, Long> anomalyID, ConcurrentHashMap<Integer, DateTime> anomalyTime) {
+    public AnomalyExpirationListener(ConcurrentHashMap<Integer, Long> anomalyID, ConcurrentHashMap<Integer, DateTime> anomalyTime)
+    {
         this.anomalyID = anomalyID;
         this.anomalyTime = anomalyTime;
         this.expiredAnomalies = new HashSet<>();
     }
 
     @Override
-    public void run() {
+    public void run()
+    {
         int anomalyLiveTime;
-        try {
+        int expirationBroadcastTime;
+        try
+        {
             anomalyLiveTime = (int) Options.getInstance().getPreference("AnomalyLiveTime", Integer.class);
-        } catch (IllegalPreferenceObjectExpected e) {
+            expirationBroadcastTime = (int) Options.getInstance().getPreference("AnomalyExpirationBroadcastTime", Integer.class);
+        }
+        catch (IllegalPreferenceObjectExpected e)
+        {
             e.printStackTrace();
             return;
         }
-        while (true) {
+        while (true)
+        {
             anomalyID.entrySet()
                     .stream()
                     .filter(entry -> !expiredAnomalies.contains(entry.getValue()))
@@ -40,20 +49,28 @@ public class AnomalyExpirationListener extends Thread {
                         DateTime anomaly = anomalyTime.get(entry.getKey());
                         DateTime now = DateTime.now();
                         int lastUpdateInSeconds = Seconds.secondsBetween(anomaly, now).getSeconds();
-                        if (lastUpdateInSeconds > anomalyLiveTime) {
+                        if (lastUpdateInSeconds > anomalyLiveTime)
+                        {
                             sendMessage(entry.getKey(), entry.getValue());
-                            expiredAnomalies.add(entry.getValue());
+                            if (lastUpdateInSeconds > expirationBroadcastTime)
+                            {
+                                expiredAnomalies.add(entry.getValue());
+                            }
                         }
                     });
-            try {
+            try
+            {
                 sleep(1000 * 60);
-            } catch (InterruptedException e) {
+            }
+            catch (InterruptedException e)
+            {
                 e.printStackTrace();
             }
         }
     }
 
-    private void sendMessage(int routeId, long anomalyId) {
+    private void sendMessage(int routeId, long anomalyId)
+    {
         AnomalyOperationProtos.AnomalyMessage message = AnomalyOperationProtos.AnomalyMessage.newBuilder()
                 .setAnomalyID(anomalyId)
                 .setRouteIdx(routeId)
@@ -63,7 +80,8 @@ public class AnomalyExpirationListener extends Thread {
         server.send(message.toByteArray());
     }
 
-    public void setServer(Server server) {
+    public void setServer(Server server)
+    {
         this.server = server;
     }
 }
