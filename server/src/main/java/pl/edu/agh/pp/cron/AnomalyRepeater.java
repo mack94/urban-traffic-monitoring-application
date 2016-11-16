66959b54ee21fa21ec2594d3a8044d5f436aef03
drzuby <dr.drzuby@gmail.com>
2016-11-16 17:43:29 +0100

    Splitting route into two parts when API returns alternative.

--- server/src/main/java/pl/edu/agh/pp/cron/AnomalyRepeater.java
+++ server/src/main/java/pl/edu/agh/pp/cron/AnomalyRepeater.java
@@ -2,28 +2,32 @@ package pl.edu.agh.pp.cron;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.model.TravelMode;
+
+import java.util.List;
+
 import org.joda.time.Instant;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import pl.edu.agh.pp.cron.utils.RequestParams;
 import pl.edu.agh.pp.detector.trackers.AnomalyTracker;
 import pl.edu.agh.pp.detector.trackers.IAnomalyTracker;
 import pl.edu.agh.pp.settings.exceptions.IllegalPreferenceObjectExpected;
-
-import java.util.List;
 
 /**
  * Created by Jakub Janusz on 12.10.2016.
  * 18:30
  * server
  */
-public class AnomalyRepeater extends Thread {
+public class AnomalyRepeater extends Thread
+{
 
     private final RequestsExecutor requestsExecutor;
     private final JSONArray routes;
     private final GeoApiContext context;
     private final IAnomalyTracker anomalyTracker;
 
-    public AnomalyRepeater(RequestsExecutor requestsExecutor, JSONArray routes, GeoApiContext context) throws IllegalPreferenceObjectExpected {
+    public AnomalyRepeater(RequestsExecutor requestsExecutor, JSONArray routes, GeoApiContext context) throws IllegalPreferenceObjectExpected
+    {
         this.requestsExecutor = requestsExecutor;
         this.routes = routes;
         this.context = context;
@@ -31,41 +35,55 @@ public class AnomalyRepeater extends Thread {
     }
 
     @Override
-    public void run() {
-        while (true) {
-            synchronized (requestsExecutor) {
+    public void run()
+    {
+        while (true)
+        {
+            synchronized (requestsExecutor)
+            {
                 List<Integer> routesIds = anomalyTracker.getCurrentAnomaliesRoutesIds();
-                for (Integer id : routesIds) {
-                    try {
+                for (Integer id : routesIds)
+                {
+                    try
+                    {
                         JSONObject route = getRoute(id);
-                        if (route != null) {
-                            String destinations[] = new String[1];
-                            String origins[] = new String[1];
-                            destinations[0] = route.get("destination").toString();
-                            origins[0] = route.get("origin").toString();
-                            String defaultWaypoints = route.getString("coords");
+                        if (route != null)
+                        {
+                            RequestParams requestParams = new RequestParams()
+                                    .withId(String.valueOf(id))
+                                    .withOrigins(new String[] { route.get("origin").toString() })
+                                    .withDestinations(new String[] { route.get("destination").toString() })
+                                    .withDeparture(Instant.now())
+                                    .withTravelMode(TravelMode.DRIVING)
+                                    .withDefaultWaypoints(route.getString("coords"));
 
-                            TravelMode travelMode = TravelMode.DRIVING;
-                            Instant departure = Instant.now();
-                            requestsExecutor.execute(String.valueOf(id), context, origins, destinations, travelMode, departure, defaultWaypoints);
+                            requestsExecutor.execute(context, requestParams);
                         }
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e)
+                    {
                         e.printStackTrace();
                     }
                 }
             }
-            try {
+            try
+            {
                 Thread.sleep(1000 * 60);
-            } catch (InterruptedException e) {
+            }
+            catch (InterruptedException e)
+            {
                 e.printStackTrace();
             }
         }
     }
 
-    private JSONObject getRoute(int id) {
-        for (int i = 0; i < routes.length(); i++) {
+    private JSONObject getRoute(int id)
+    {
+        for (int i = 0; i < routes.length(); i++)
+        {
             JSONObject route = routes.getJSONObject(i);
-            if (Integer.valueOf(route.getString("id")) == id) {
+            if (Integer.valueOf(route.getString("id")) == id)
+            {
                 return route;
             }
         }
