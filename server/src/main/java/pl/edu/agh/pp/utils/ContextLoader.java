86c1f04276573e1faf8a2adaffc9abe39314689d
Dawid <dtrylinski@hotmail.com>
2016-11-21 19:36:13 +0100

    Added help command, defualt command and change api key command to server

--- server/src/main/java/pl/edu/agh/pp/utils/ContextLoader.java
+++ server/src/main/java/pl/edu/agh/pp/utils/ContextLoader.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -19,12 +21,15 @@ public class ContextLoader {
     private Properties properties;
     private String propertiesFileName = "/config.properties";
     private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+    private static List<GeoApiContext> contextList = new ArrayList<>();
 
     public GeoApiContext geoApiContextLoader() throws IOException {
         properties = loadAppProperties();
         String apiKey = properties.getProperty("ApiKey");
 
-        return new GeoApiContext().setApiKey(apiKey);
+        GeoApiContext context = new GeoApiContext().setApiKey(apiKey);
+        contextList.add(context);
+        return context;
     }
 
     private Properties loadAppProperties() throws IOException {
@@ -36,5 +41,11 @@ public class ContextLoader {
         properties.load(configStream);
 
         return properties;
+    }
+
+    public static void changeApiKey(String newApiKey){
+        for(GeoApiContext context: contextList){
+            context.setApiKey(newApiKey);
+        }
     }
 }
