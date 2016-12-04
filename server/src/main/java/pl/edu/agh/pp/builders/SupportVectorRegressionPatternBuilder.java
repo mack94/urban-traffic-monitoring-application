160638c77b58ccdcec036f9569a8eaf11d9e1cc1
Krzysztof <krzysiu@smsnet.pl>
2016-12-04 16:24:31 +0100

    Deleted condition, on which anomalies under baseline were reported.

--- server/src/main/java/pl/edu/agh/pp/builders/SupportVectorRegressionPatternBuilder.java
+++ server/src/main/java/pl/edu/agh/pp/builders/SupportVectorRegressionPatternBuilder.java
@@ -207,7 +207,7 @@ public class SupportVectorRegressionPatternBuilder implements Strategy {
         logger.info(String.valueOf(predictedTravelDurationMaximum + errorDelta));
 
 
-        if ((travelDuration > predictedTravelDurationMaximum + errorDelta) || (travelDuration < predictedTravelDurationMinimum - errorDelta)) {
+        if ((travelDuration > predictedTravelDurationMaximum + errorDelta) ) {
 
             if (travelDuration > predictedTravelDuration + errorDelta)
                 errorRate = travelDuration / predictedTravelDuration;
