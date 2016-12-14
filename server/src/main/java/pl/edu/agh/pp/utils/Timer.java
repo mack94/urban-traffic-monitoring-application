d2acb0a7c699265bc6e523302645f5aca037c961
drzuby <dr.drzuby@gmail.com>
2016-12-14 21:23:46 +0100

    Changed message after generating cron waiting time - now interval is displayed.

--- server/src/main/java/pl/edu/agh/pp/utils/Timer.java
+++ server/src/main/java/pl/edu/agh/pp/utils/Timer.java
@@ -1,11 +1,6 @@
 package pl.edu.agh.pp.utils;
 
 import ch.qos.logback.classic.Logger;
-import org.joda.time.DateTime;
-import org.slf4j.LoggerFactory;
-import pl.edu.agh.pp.settings.IOptions;
-import pl.edu.agh.pp.settings.Options;
-import pl.edu.agh.pp.settings.PreferencesNamesHolder;
 
 import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
@@ -13,27 +8,38 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
+import org.joda.time.DateTime;
+import org.slf4j.LoggerFactory;
+import pl.edu.agh.pp.settings.IOptions;
+import pl.edu.agh.pp.settings.Options;
+import pl.edu.agh.pp.settings.PreferencesNamesHolder;
+
 /**
  * Created by Maciej on 15.05.2016.
  * 16:05
  * Project: 1.
  */
-public class Timer {
+public class Timer
+{
 
     private final static Timer INSTANCE = new Timer();
     private static Logger logger = (Logger) LoggerFactory.getLogger(Timer.class.getClass());
     private static DayOfWeek dayOfWeek = DayOfWeek.of(DateTime.now().getDayOfWeek());
     private final IOptions options = Options.getInstance();
 
-    private Timer() {
+    private Timer()
+    {
     }
 
-    public static Timer getInstance() {
+    public static Timer getInstance()
+    {
         return INSTANCE;
     }
 
-    public long getWaitingTime(Calendar currentCalendar) {
-        try {
+    public long getWaitingTime(Calendar currentCalendar)
+    {
+        try
+        {
             String string1 = (String) options.getPreference(PreferencesNamesHolder.DAY_SHIFT_START, String.class);
             Date time1 = new SimpleDateFormat("HH:mm:ss").parse(string1);
             Calendar calendar1 = Calendar.getInstance();
@@ -54,25 +60,33 @@ public class Timer {
 
             Date x = currentCalendar.getTime();
             Random random = new Random();
-            if (x.after(calendar1.getTime()) && x.before(calendar2.getTime())) {
+            if (x.after(calendar1.getTime()) && x.before(calendar2.getTime()))
+            {
                 int from = (int) Options.getInstance().getPreference(PreferencesNamesHolder.DAY_SHIFT_FREQUENCY_FROM, Integer.class);
                 int to = (int) Options.getInstance().getPreference(PreferencesNamesHolder.DAY_SHIFT_FREQUENCY_TO, Integer.class);
                 int diff = to - from;
-                System.out.println("DAY SHIFT-----------------------------------------------");
-                return random.nextInt(diff) + from;
-            } else {
+                int waitingTime = random.nextInt(diff) + from;
+                logger.info("DAY SHIFT - waiting time is {} seconds", waitingTime);
+                return waitingTime;
+            }
+            else
+            {
                 int from = (int) Options.getInstance().getPreference(PreferencesNamesHolder.NIGHT_SHIFT_FREQUENCY_FROM, Integer.class);
                 int to = (int) Options.getInstance().getPreference(PreferencesNamesHolder.NIGHT_SHIFT_FREQUENCY_TO, Integer.class);
                 int diff = to - from;
-                System.out.println("NIGHT SHIFT----------------------------------------------");
-                if (!Timer.dayOfWeek.equals(DayOfWeek.of(DateTime.now().getDayOfWeek()))) {
+                int waitingTime = random.nextInt(diff) + from;
+                logger.info("NIGHT SHIFT- waiting time is {} seconds", waitingTime);
+                if (!Timer.dayOfWeek.equals(DayOfWeek.of(DateTime.now().getDayOfWeek())))
+                {
                     Timer.dayOfWeek = DayOfWeek.of(DateTime.now().getDayOfWeek());
                     logger.info("New day. GC will run!");
                     System.gc();
                 }
-                return random.nextInt(diff) + from;
+                return waitingTime;
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             logger.error("Error during calculating time to download traffic.");
         }
         return 720_000;
